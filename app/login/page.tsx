7ae544c883ba19a7d822c776bfbba3f8cc23c6d8
fti-skumar <skumar@ftilab.com>
2025-04-04 14:24:19 +0900

    Add prop and update animation

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled, { keyframes } from 'styled-components';
 import LoginIllustration from '../assets/login-security.svg';
 import Image from 'next/image';
@@ -19,42 +19,11 @@ const updatWidth = keyframes`
 `;
 
 const Container = styled.div`
-  width: 100%;
-  max-width: 411px;
-  height: 560px;
-  padding: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #94b7ff;
-  box-shadow: rgba(50, 69, 82, 0.15) 0px 20px 30px 0px;
-  border-radius: 15px;
-  overflow: hidden;
   animation: ${updatWidth} 0.5s ease-in-out forwards 0.5s;
 `;
 
 const LeftContainer = styled.div`
-  width: 0;
-  height: 100%;
-  padding: 80px 0;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  gap: 20px;
-  opacity: 0;
   animation: ${fadeIn} 0.5s ease-in-out forwards 1s;
-`;
-
-const RightContainer = styled.div`
-  width: 418px;
-  height: 100%;
-  background-color: #fff;
-  background-color: #94b7ff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 15px;
-  margin-left: auto;
 `;
 
 const LoginHeading = styled.h1`
@@ -79,17 +48,24 @@ const LoginButton = styled(Button)`
   margin-top: 15px;
 `;
 
-const Login = () => {
+type LoginProps = {
+  isLoggedIn: boolean;
+}
+
+const Login: React.FC<LoginProps> = ({isLoggedIn}) => {
   const [showPassword, setShowPassword] = useState(false);
-  // const [isHidden, setIsHidden] = useState(true);
+
+  useEffect(() => {
+    console.log(isLoggedIn, 'abcdl');
+  }, [isLoggedIn]);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   }
 
   return (
-  <Container>
-    <LeftContainer>
+  <Container className='w-full h-[560px] max-w-[411px] flex justify-center items-center padding-[10px] border-[1px] border-[#94b7ff] shadow-[rgba(50,69,82,0.15)_0px_20px_30px_0px] rounded-[15px] overflow-hidden'>
+    <LeftContainer className='opacity-[0] w-[0] h-full flex items-center gap-[20px] flex-col p-[80px_0]'>
       <LoginHeading>Hello NextJS!</LoginHeading>
       <InputContainer>
         <Label htmlFor='username' className='text-[16px] text-[#263238]'>Username</Label>
@@ -104,16 +80,14 @@ const Login = () => {
       </InputContainer>
       <LoginButton className='hover:opacity-85 rounded-[5px]' onClick={() => {}}>Login</LoginButton>
     </LeftContainer>
-    <RightContainer>
+    <div className='w-[418px] h-full bg-[#94b7ff] flex justify-center items-center rounded-[15px] ml-auto'>
       <Image
+        loading='eager'
         src={LoginIllustration}
         alt='Login Illustration'
         style={{ width: '100%', height: '100%' }}
-        onLoad={() => {
-          console.log('loaded');
-        }}
       />
-    </RightContainer>
+    </div>
   </Container>
   );
 };
