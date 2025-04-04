220aa51eda4807392eeae52723772ac014e625ba
fti-skumar <skumar@ftilab.com>
2025-04-04 16:27:50 +0900

    Fix animation 2

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -86,6 +86,7 @@ export default function Login(props: LoginProps) {
           src={LoginIllustration}
           alt='Login Illustration'
           style={{ width: '100%', height: '100%' }}
+          unoptimized
         />
       </div>
     </Container>
